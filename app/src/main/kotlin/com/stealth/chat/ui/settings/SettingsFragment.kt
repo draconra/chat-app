b2742e7139b408e1a58cc155ac1e74e9a4b65686
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2025-04-15 20:29:21 +0700

    remove: unused layout

--- app/src/main/kotlin/com/stealth/chat/ui/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/stealth/chat/ui/settings/SettingsFragment.kt
@@ -1,14 +1,9 @@
 package com.stealth.chat.ui.settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.stealth.chat.databinding.FragmentNotificationsBinding
 import com.stealth.chat.ui.settings.uicomponent.SettingsHost
 import com.stealth.chat.ui.theme.ChatAppTheme
 
