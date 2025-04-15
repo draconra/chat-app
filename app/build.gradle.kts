b14d45bb0d929b45a291505511f4f955aed69166
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2025-04-15 22:37:33 +0700

    update: build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stealth.chat"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = project.findProperty("BUILD_NUMBER")?.toString()?.toIntOrNull() ?: 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
