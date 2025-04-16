72608eacde927dcd9bbe57b75312c056585d565b
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2025-04-16 14:15:19 +0700

    add: new page in settings

--- app/src/main/kotlin/com/stealth/chat/ui/settings/uicomponent/SettingsScreen.kt
+++ app/src/main/kotlin/com/stealth/chat/ui/settings/uicomponent/SettingsScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.stealth.chat.ui.settings.uicomponent
 
 import androidx.compose.foundation.clickable
@@ -11,10 +13,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -31,14 +37,24 @@ fun SettingsScreen(
     val versionName = BuildConfig.VERSION_NAME
     val buildNumber = "14"
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Spacer(modifier = Modifier.size(15.dp))
-        Text("Settings", style = MaterialTheme.typography.titleLarge)
-        Text("Version: $versionName (Build $buildNumber)", style = MaterialTheme.typography.bodyMedium)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Settings") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+            Text("Version: $versionName (Build $buildNumber)", style = MaterialTheme.typography.bodyMedium)
 
-        Spacer(Modifier.height(16.dp))
-        SettingItem(Icons.Default.Build, "Change Base URL: $baseUrl", onEditBaseUrl)
-        SettingItem(Icons.Default.Person, "Change Username: $username", onEditUsername)
+            Spacer(Modifier.height(16.dp))
+            SettingItem(Icons.Default.Build, "Change Base URL: $baseUrl", onEditBaseUrl)
+            SettingItem(Icons.Default.Person, "Change Username: $username", onEditUsername)
+        }
     }
 }
 
