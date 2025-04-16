5e1c2f8e58c229a42139367be06092dd162b09df
Aditya Sumardi <adityasumardi@sumtechconsulting.co.id>
2025-04-16 08:46:01 +0700

    fix: error in build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.firebase)
-    kotlin("kapt")
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
